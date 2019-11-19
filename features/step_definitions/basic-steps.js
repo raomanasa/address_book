dc95c66fd4068c4532b1df522ec3dd817f6ba73c
your_name <your@email.com>
2019-11-19 13:27:23 +0100

    adds main.features

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -9,3 +9,34 @@ Then("I should see {string}", function(string) {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
 });
+const { Given, Then, When } = require('cucumber')
+
+Given('I visit the site', async function() {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should see {string}', async function(content) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+When('I click {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I fill in {string} with {string}', async function(string, string2) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should have {int} contact in my address book', async function(int) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
+
+Then('I should not see {string}', async function(string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending'
+})
